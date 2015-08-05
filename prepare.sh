57632216978da547448f9219c1eb52e2f4109e72
ericmarcos <ericmarcos.p@gmail.com>
2015-08-05 12:35:47 +0200

    added a script to automate android notification icons copy

---
+++ prepare.sh
@@ -0,0 +1,7 @@
+ionic prepare
+cp resources/android/notification/drawable-ldpi-ic_stat_notify.png platforms/android/res/drawable-ldpi/ic_stat_notify.png
+cp resources/android/notification/drawable-mdpi-ic_stat_notify.png platforms/android/res/drawable-mdpi/ic_stat_notify.png
+cp resources/android/notification/drawable-hdpi-ic_stat_notify.png platforms/android/res/drawable-hdpi/ic_stat_notify.png
+cp resources/android/notification/drawable-xhdpi-ic_stat_notify.png platforms/android/res/drawable-xhdpi/ic_stat_notify.png
+cp resources/android/notification/drawable-xxhdpi-ic_stat_notify.png platforms/android/res/drawable-xxhdpi/ic_stat_notify.png
+cp resources/android/notification/drawable-xxxhdpi-ic_stat_notify.png platforms/android/res/drawable-xxxhdpi/ic_stat_notify.png
