af336fdeb2ea640869b4d7f2e2d0ba639da2800c
ericmarcos <ericmarcos.p@gmail.com>
2015-08-07 13:35:52 +0200

    bumped to version 1.0.2

--- prepare.sh
+++ prepare.sh
@@ -5,3 +5,5 @@ cp resources/android/notification/drawable-hdpi-ic_stat_notify.png platforms/and
 cp resources/android/notification/drawable-xhdpi-ic_stat_notify.png platforms/android/res/drawable-xhdpi/ic_stat_notify.png
 cp resources/android/notification/drawable-xxhdpi-ic_stat_notify.png platforms/android/res/drawable-xxhdpi/ic_stat_notify.png
 cp resources/android/notification/drawable-xxxhdpi-ic_stat_notify.png platforms/android/res/drawable-xxxhdpi/ic_stat_notify.png
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../android-signature/teamwin-release.keystore platforms/android/build/outputs/apk/android-x86-release-unsigned.apk teamwin
+ls -la
